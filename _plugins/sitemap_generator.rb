030364eb7eb5495774202dace3eb9a08ce8fafec
Tyler Wayne <tyler.wayne.dev@icloud.com>
2013-03-06 21:53:59 -0700

    Major changes to incorporate Jekyll.
    - Extracted index.html into default template.
    - Applied default template to home, about, effective-d.
    - Removed JS navbar solution.
    - Altered footer text.

--- _plugins/sitemap_generator.rb
+++ _plugins/sitemap_generator.rb
@@ -40,7 +40,7 @@ require 'rexml/document'
 module Jekyll
 
   # Change MY_URL to reflect the site you are using
-  MY_URL = "http://www.mysite.com"
+  MY_URL = "http://tylerwayne.github.com"
 
   # Change SITEMAP_FILE_NAME if you would like your sitemap file
   # to be called something else
@@ -51,7 +51,7 @@ module Jekyll
 
   # Any files that include posts, so that when a new post is added, the last
   # modified date of these pages should take that into account
-  PAGES_INCLUDE_POSTS = ["index.html"]
+  PAGES_INCLUDE_POSTS = []
 
   # Custom variable names for changefreq and priority elements
   # These names are used within the YAML Front Matter of pages or posts
